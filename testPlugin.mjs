3f031c51bbce7ad2504fcea84a931c3e74eac2b3
spencerHT <spencerHT@github.com>
2024-05-07 14:04:56 +0800

    chore: update

--- testPlugin.mjs
+++ testPlugin.mjs
@@ -9,14 +9,12 @@ export class TestPlugin {
     this.options = options;
   }
 
-
   apply(compiler) {
     compiler.hooks.compilation.tap(PLUGIN_NAME, compilation => {
       HtmlWebpackPlugin
         .getHooks(compilation)
         .alterAssetTags.tapAsync(PLUGIN_NAME, (data, cb) => {
 
-          // 只考虑一个 chunk 的情况
           const currentEntry = data.plugin.options.entryName || 'main';
 
           const entryChunks = compilation.entrypoints.get(currentEntry).chunks;
